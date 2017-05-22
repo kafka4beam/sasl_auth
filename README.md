73c503e23d09aade7f195293944c9e1642204259
Max Davidenko <armwebdev@gmail.com>
2017-05-22 18:36:16 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+sasl_auth
+=====
+
+sasl_auth is a simple wrapper for cyrus sasl library (https://cyrusimap.org/docs/cyrus-sasl/2.1.23/programming.php)
+It helps to implement SASL GSSAPI auth mechanism support in your erlang application.
+
+Dependencies
+-----
+
+libsasl2, libsasl2-dev, libkrb5, libkrb5-dev should be installed to build and use sasl_auth
+
+Build
+-----
+
+    $ rebar3 compile
+
+Use
+-----
+
+sasl_auth used in https://github.com/ElMaxo/brod_gssapi - GSSAPI auth backend for brod (https://github.com/klarna/brod) Apache Kafka client library for Erlang/Elixir. See usage details here
---
+++ src/sasl_auth.erl
@@ -0,0 +1,45 @@
+%% @doc
+%% Wrapper for cyrus sasl library for GSSAPI mechanism support in erlang applications. Each function (except kinit) corresponds to
+%% functions at libsasl2
+%% @end
+-module(sasl_auth).
+
+%% API
+-export([init/0, kinit/2, sasl_client_init/0, sasl_client_new/3, sasl_listmech/0, sasl_client_start/0, sasl_client_step/1, sasl_errdetail/0]).
+-on_load(init/0).
+
+init() ->
+    NifLib =
+        case code:priv_dir(sasl_auth) of
+            {error, bad_name} ->
+                case code:which(?MODULE) of
+                    Filename when is_list(Filename) ->
+                        filename:join([filename:dirname(Filename), "../priv", "sasl_auth"]);
+                    _ ->
+                        filename:join("../priv", "sasl_auth")
+                end;
+            Dir ->
+                filename:join(Dir, "sasl_auth")
+        end,
+    erlang:load_nif(NifLib, 0).
+
+-spec kinit(Keytab :: binary(), Principal :: binary()) -> {ok, Result :: string()} | {error, Reason :: string()}.
+kinit(_, _) -> exit(nif_library_not_loaded).
+
+-spec sasl_client_init() -> SaslCode :: integer() | {error, Reason :: string()}.
+sasl_client_init() -> exit(nif_library_not_loaded).
+
+-spec sasl_client_new(ServiceName :: binary(), Host :: binary(), Principal :: binary()) -> SaslCode :: integer() | {error, Reason :: string()}.
+sasl_client_new(_, _, _) -> exit(nif_library_not_loaded).
+
+-spec sasl_listmech() -> SupportedMechs :: string() | {error, Reason :: string()}.
+sasl_listmech() -> exit(nif_library_not_loaded).
+
+-spec sasl_client_start() -> {SaslRes :: integer(), Token :: string()} | {error, Reason :: string()}.
+sasl_client_start() -> exit(nif_library_not_loaded).
+
+-spec sasl_client_step(Token :: binary()) ->  {SaslRes :: integer(), Token :: string()} | {error, Reason :: string()}.
+sasl_client_step(_) -> exit(nif_library_not_loaded).
+
+-spec sasl_errdetail() -> ErrorDescription :: string().
+sasl_errdetail() ->  exit(nif_library_not_loaded).
