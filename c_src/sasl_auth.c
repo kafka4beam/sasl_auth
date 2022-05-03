ce1b80bd4550f94af22c0e65b4c73707727ef768
Kjell Winblad <kjellwinblad@gmail.com>
2022-05-03 18:39:45 +0200

    style: make sure spaces and tabs are not both used for indentation
    Before this commit my C compiler printed warnings about inconsistent and
    misleading indentation. Also it is more practical to consistently use
    spaces instead of a mix of spaces and tabs. This commit makes sure that
    only spaces are used for indentation.

--- c_src/sasl_auth.c
+++ c_src/sasl_auth.c
@@ -12,15 +12,15 @@
 // Structure for kerberos authentication
 struct kebab
 {
-  krb5_error_code error;
-  krb5_principal  principal;
-  krb5_context    context;
-  krb5_creds      creds;
+    krb5_error_code error;
+    krb5_principal  principal;
+    krb5_context    context;
+    krb5_creds      creds;
 };
 
 typedef struct sasl_state_s {
-        sasl_conn_t *conn;
-        sasl_callback_t callbacks[16];
+    sasl_conn_t *conn;
+    sasl_callback_t callbacks[16];
 } sasl_state_t;
 
 // SASL connection state
@@ -48,8 +48,8 @@ ERL_NIF_TERM get_atom(ErlNifEnv* env, const char* atom_name)
 
 ERL_NIF_TERM make_error(ErlNifEnv* env, char *tag)
 {
-	ERL_NIF_TERM return_atom = get_atom(env, "error");
-	ERL_NIF_TERM reason = enif_make_string(env, tag, ERL_NIF_LATIN1);
+    ERL_NIF_TERM return_atom = get_atom(env, "error");
+    ERL_NIF_TERM reason = enif_make_string(env, tag, ERL_NIF_LATIN1);
 
     return enif_make_tuple2(env, return_atom, reason);
 }
@@ -67,12 +67,12 @@ ERL_NIF_TERM error_and_exit(ErlNifEnv* env, struct kebab *kebab, char *tag)
         krb5_free_error_message(kebab->context, krb_error_msg);
 
     if (kebab->error == 0)
-		return_atom = get_atom(env, "ok");
+        return_atom = get_atom(env, "ok");
     else if (kebab->error == KRB5KDC_ERR_C_PRINCIPAL_UNKNOWN ||
-           kebab->error == KRB5KRB_AP_ERR_BAD_INTEGRITY ||
-           kebab->error == KRB5KDC_ERR_PREAUTH_FAILED ||
-           kebab->error == KRB5KDC_ERR_KEY_EXPIRED
-          )
+            kebab->error == KRB5KRB_AP_ERR_BAD_INTEGRITY ||
+            kebab->error == KRB5KDC_ERR_PREAUTH_FAILED ||
+            kebab->error == KRB5KDC_ERR_KEY_EXPIRED
+            )
         return_atom = get_atom(env, "refused");
     else
         return_atom = get_atom(env, "error");
@@ -85,36 +85,36 @@ ERL_NIF_TERM error_and_exit(ErlNifEnv* env, struct kebab *kebab, char *tag)
             krb5_free_principal(kebab->context, kebab->principal);
 
         krb5_free_context(kebab->context);
-  }
+    }
 
     return enif_make_tuple2(env, return_atom, error_message);
 }
 
 static int sasl_cyrus_cb_getsimple(void *context, int id, const char **result, unsigned *len)
 {
-	switch (id)
-	{
-		case SASL_CB_USER:
-			*result = context;
-		case SASL_CB_AUTHNAME:
-	        *result = context;
-	    break;
-		default:
-	        *result = NULL;
-	    break;
-	}
+    switch (id)
+    {
+        case SASL_CB_USER:
+            *result = context;
+        case SASL_CB_AUTHNAME:
+            *result = context;
+            break;
+        default:
+            *result = NULL;
+            break;
+    }
 
-	if (len)
-		*len = *result ? strlen(*result) : 0;
+    if (len)
+        *len = *result ? strlen(*result) : 0;
 
-	return *result ? SASL_OK : SASL_FAIL;
+    return *result ? SASL_OK : SASL_FAIL;
 }
 
 static ERL_NIF_TERM sasl_cli_init(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-	int result;
-	result = sasl_client_init(NULL);
-	return enif_make_int(env, result);
+    int result;
+    result = sasl_client_init(NULL);
+    return enif_make_int(env, result);
 }
 
 static ERL_NIF_TERM sasl_cli_new(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
@@ -126,7 +126,7 @@ static ERL_NIF_TERM sasl_cli_new(ErlNifEnv* env, int argc, const ERL_NIF_TERM ar
     if(state) {
         if(state ->conn) 
             sasl_dispose(&state->conn);
-        
+
         free(state);
     }
 
@@ -142,33 +142,33 @@ static ERL_NIF_TERM sasl_cli_new(ErlNifEnv* env, int argc, const ERL_NIF_TERM ar
     principal.data[principal.size] = '\0';
 
     sasl_callback_t callbacks[16] =
-    	{
-    		{ SASL_CB_USER,         (void *)sasl_cyrus_cb_getsimple, principal.data },
-            { SASL_CB_AUTHNAME,     (void *)sasl_cyrus_cb_getsimple, principal.data },
-            { SASL_CB_LIST_END }
-    	};
+    {
+        { SASL_CB_USER,         (void *)sasl_cyrus_cb_getsimple, principal.data },
+        { SASL_CB_AUTHNAME,     (void *)sasl_cyrus_cb_getsimple, principal.data },
+        { SASL_CB_LIST_END }
+    };
 
     memcpy(state->callbacks, callbacks, sizeof(callbacks));
 
     int result;
-	result = sasl_client_new((const char *)service.data, (const char *)host.data, NULL, NULL, state->callbacks, 0, &state->conn);
-	return enif_make_int(env, result);
+    result = sasl_client_new((const char *)service.data, (const char *)host.data, NULL, NULL, state->callbacks, 0, &state->conn);
+    return enif_make_int(env, result);
 }
 
 static ERL_NIF_TERM sasl_list_mech(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-	const char *avail_mechs;
-	sasl_listmech(state->conn, NULL, NULL, " ", NULL, &avail_mechs, NULL, NULL);
-	return enif_make_string(env, avail_mechs, ERL_NIF_LATIN1);
+    const char *avail_mechs;
+    sasl_listmech(state->conn, NULL, NULL, " ", NULL, &avail_mechs, NULL, NULL);
+    return enif_make_string(env, avail_mechs, ERL_NIF_LATIN1);
 }
 
 static ERL_NIF_TERM sasl_cli_start(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-	int result;
-	const char *out, *mech;
-	unsigned int outlen;
-	result = sasl_client_start(state->conn, "GSSAPI", NULL, &out, &outlen, &mech);
-	return enif_make_tuple2(env, enif_make_int(env, result), enif_make_string_len(env, out, outlen, ERL_NIF_LATIN1));
+    int result;
+    const char *out, *mech;
+    unsigned int outlen;
+    result = sasl_client_start(state->conn, "GSSAPI", NULL, &out, &outlen, &mech);
+    return enif_make_tuple2(env, enif_make_int(env, result), enif_make_string_len(env, out, outlen, ERL_NIF_LATIN1));
 }
 
 static ERL_NIF_TERM sasl_cli_step(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
@@ -181,28 +181,28 @@ static ERL_NIF_TERM sasl_cli_step(ErlNifEnv* env, int argc, const ERL_NIF_TERM a
     if (!enif_inspect_binary(env, argv[0], &challenge))
         return enif_make_badarg(env);
 
-	int result;
-	sasl_interact_t *interact = NULL;
+    int result;
+    sasl_interact_t *interact = NULL;
     const char *out;
     unsigned int outlen;
     char* in = NULL;
     if (challenge.size > 0)
     {
-		in = (char*)malloc(challenge.size);
-		memcpy(in, challenge.data, challenge.size);
-	}
-	result = sasl_client_step(state->conn,
-                             in, challenge.size,
-                             &interact,
-                             &out, &outlen);
-	free(in);
+        in = (char*)malloc(challenge.size);
+        memcpy(in, challenge.data, challenge.size);
+    }
+    result = sasl_client_step(state->conn,
+            in, challenge.size,
+            &interact,
+            &out, &outlen);
+    free(in);
 
-	return enif_make_tuple2(env, enif_make_int(env, result), enif_make_string_len(env, out, outlen, ERL_NIF_LATIN1));
+    return enif_make_tuple2(env, enif_make_int(env, result), enif_make_string_len(env, out, outlen, ERL_NIF_LATIN1));
 }
 
 static ERL_NIF_TERM sasl_error(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-	return enif_make_string(env, sasl_errdetail(state->conn), ERL_NIF_LATIN1);
+    return enif_make_string(env, sasl_errdetail(state->conn), ERL_NIF_LATIN1);
 }
 
 static ERL_NIF_TERM kinit(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
@@ -237,7 +237,7 @@ static ERL_NIF_TERM kinit(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
     if ((kebab.error = krb5_cc_default(kebab.context, &defcache)))
     {
         krb5_kt_close(kebab.context, ktHnd);
-    	return error_and_exit(env, &kebab, "krb5_cc_default");
+        return error_and_exit(env, &kebab, "krb5_cc_default");
     }
 
     krb5_get_init_creds_opt *options = NULL;
@@ -255,14 +255,14 @@ static ERL_NIF_TERM kinit(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
         return error_and_exit(env, &kebab, "krb5_get_init_creds_opt_set_out_ccache");
     }
 
-	if ((kebab.error = krb5_get_init_creds_keytab(kebab.context,&kebab.creds,kebab.principal,ktHnd,0,NULL,options)))
-	{
-	    krb5_cc_close(kebab.context, defcache);
-		krb5_kt_close(kebab.context, ktHnd);
-		return error_and_exit(env, &kebab, "krb5_get_init_creds_keytab");
-	}
-	krb5_cc_close(kebab.context, defcache);
-	krb5_kt_close(kebab.context, ktHnd);
+    if ((kebab.error = krb5_get_init_creds_keytab(kebab.context,&kebab.creds,kebab.principal,ktHnd,0,NULL,options)))
+    {
+        krb5_cc_close(kebab.context, defcache);
+        krb5_kt_close(kebab.context, ktHnd);
+        return error_and_exit(env, &kebab, "krb5_get_init_creds_keytab");
+    }
+    krb5_cc_close(kebab.context, defcache);
+    krb5_kt_close(kebab.context, ktHnd);
 
     return error_and_exit(env, &kebab, NULL);
 }
@@ -272,20 +272,20 @@ void unload(ErlNifEnv* env, void* priv_data)
     if(state) {
         if(state ->conn) 
             sasl_dispose(&state->conn);
-        
+
         free(state);
     }
 }
 
 static ErlNifFunc nif_funcs[] =
 {
-	{"sasl_client_init", 0, sasl_cli_init},
-	{"sasl_client_new", 3, sasl_cli_new},
-	{"sasl_listmech", 0, sasl_list_mech},
-	{"sasl_client_start", 0, sasl_cli_start},
-	{"sasl_client_step", 1, sasl_cli_step},
-	{"sasl_errdetail", 0, sasl_error},
-	{"kinit", 2, kinit}
+    {"sasl_client_init", 0, sasl_cli_init},
+    {"sasl_client_new", 3, sasl_cli_new},
+    {"sasl_listmech", 0, sasl_list_mech},
+    {"sasl_client_start", 0, sasl_cli_start},
+    {"sasl_client_step", 1, sasl_cli_step},
+    {"sasl_errdetail", 0, sasl_error},
+    {"kinit", 2, kinit}
 };
 
 ERL_NIF_INIT(sasl_auth, nif_funcs, NULL, NULL, NULL, &unload)
