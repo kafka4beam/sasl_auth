b0c77478cd7bf9389d6afafd3cb4420ddb670c34
Kjell Winblad <kjellwinblad@gmail.com>
2022-05-04 13:23:45 +0200

    test: add scripts to run tests with address sanitizer

---
+++ test/address_sanitizer_run_eunit.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+echo "============================================"
+echo "Note that the \"test/address_sanitizer_setup.sh\" has to run"
+echo "successfully at least once before this script will work."
+echo "============================================"
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd "$SCRIPT_DIR/../otp"
+
+export ERL_TOP=`pwd`
+export PATH=$ERL_TOP/bin:$PATH
+    
+cd "$SCRIPT_DIR/.."
+export MEMSAN_DEBUG=1
+
+cp "$ERL_TOP/lib/erl_interface/obj/x86_64-pc-linux-gnu/libei.a" c_src/
+
+rebar3 as addr_san_test,test compile
+
+echo "============================================"
+echo "Running the eunit test with address sanitizer"
+echo "============================================"
+
+export ASAN_LOG_DIR=`pwd`/asan_logs
+
+(rm -rf "$ASAN_LOG_DIR" || true)
+
+mkdir "$ASAN_LOG_DIR"
+
+# ASAN_OPTIONS=intercept_tls_get_addr=0 is a workaround for a bug in address sanitizer that we hit
+# https://github.com/google/sanitizers/issues/1322
+# https://github.com/neovim/neovim/pull/17213
+
+# ASAN_OPTIONS=intercept_tls_get_addr=0
+cerl -asan -noshell -pa _build/addr_san_test+test/lib/sasl_auth/test -pa _build/addr_san_test+test/lib/sasl_auth/ebin -eval "sasl_auth_tests:test(),erlang:halt()" 
+
+echo "============================================"
+echo "The address sanitizer log (located in \"$ASAN_LOG_DIR\") will now be printed:"
+echo "============================================"
+
+cat "$ASAN_LOG_DIR/`ls "$ASAN_LOG_DIR"`"
+
--- test/sasl_auth_tests.erl
+++ test/sasl_auth_tests.erl
@@ -4,5 +4,22 @@
 -include_lib("eunit/include/eunit.hrl").
 
 
-nif_lib_should_load_test() ->
-    sasl_auth:kinit(<<"something">>, <<"something">>).
+wrap_setup_cleanup(TestCases) ->
+    [
+     {setup,
+     fun setup_nif/0,
+     fun cleanup_nif/1,
+     TestCases}
+    ].
+
+nif_lib_should_load_t() ->
+    [?_assert(hej =/= sasl_auth:kinit(<<"something">>, <<"something">>))].
+
+nif_lib_should_load_test_() -> wrap_setup_cleanup(nif_lib_should_load_t()).
+
+setup_nif() ->
+    ok.
+
+cleanup_nif(_) ->
+    true = code:delete(sasl_auth),
+    true = code:soft_purge(sasl_auth).
