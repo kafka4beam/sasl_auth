78b2d5b7ae28fa344f249652417e5b3ad2245175
zmstone <zmstone@gmail.com>
2024-08-26 17:16:21 +0200

    test: make int-test run concurrently

--- scripts/int_test.erl
+++ scripts/int_test.erl
@@ -2,52 +2,75 @@
 
 -feature(maybe_expr, enable).
 
--export([start/0]).
-
+-export([start/0, start/1, start/2]).
 
 start() ->
-    case atom_to_list(node()) of
+    process_flag(trap_exit, true),
+    start(1).
+
+start(Count) ->
+    start(Count, <<>>).
+
+start(Count, CCname) ->
+    Pids = case atom_to_list(node()) of
         "cli" ++ _ ->
-            run_cli();
+            start_loop(fun(I) -> run_cli(I, CCname) end, Count);
         "srv" ++ _ ->
-            run_srv()
+            start_loop(fun(I) -> run_srv(I, CCname) end, Count)
+    end,
+    wait_for_pids_exit(Pids).
+
+start_loop(StartFn, Count) ->
+    IDs = lists:seq(1, Count),
+    lists:map(fun(I) -> spawn_link(fun() -> StartFn(I) end) end, IDs).
+
+wait_for_pids_exit([]) ->
+    ok;
+wait_for_pids_exit([Pid | Pids]) ->
+    receive
+        {'EXIT', Pid, normal} ->
+            wait_for_pids_exit(Pids);
+        {'EXIT', Pid, Error} ->
+            exit(Error)
     end.
 
-run_cli() ->
+run_cli(ID, CCname) ->
     Service = env("SERVICE"),
     maybe
-        ok = sasl_auth:kinit("cli.keytab", env("CLI_PRINC")),
-        io:format("done kinit~n", []),
+        ok = sasl_auth:kinit("cli.keytab", env("CLI_PRINC"), CCname),
+        io:format("[~p] done kinit~n", [ID]),
         {ok, C} ?= sasl_auth:client_new(Service, env("SRV"), env("CLI_PRINC"), env("CLI_NAME")),
-        Pid = wait_for_server(),
+        Pid = wait_for_server(srv_name(ID)),
         {ok, AvaialbeMecs} ?= sasl_auth:client_listmech(C),
         true = lists:member(<<"GSSAPI">>, AvaialbeMecs),
         {ok, {sasl_continue, CT1}} ?= sasl_auth:client_start(C),
-        ST1 = send_and_recv(Pid, ct1, CT1),
-        io:format("got st1~n", []),
+        ST1 = send_and_recv(ID, Pid, ct1, CT1),
+        io:format("[~p] got st1~n", [ID]),
         {ok, {sasl_continue, CT2}} ?= sasl_auth:client_step(C, ST1),
-        ST2 = send_and_recv(Pid, ct2, CT2),
-        io:format("got st2~n", []),
+        ST2 = send_and_recv(ID, Pid, ct2, CT2),
+        io:format("[~p] got st2~n", [ID]),
         {ok, {sasl_ok, CT3}} ?= sasl_auth:client_step(C, ST2),
-        io:format("sasl_ok: ~0p~n", [CT3]),
+        io:format("[~p] sasl_ok: ~0p~n", [ID, CT3]),
         _ = erlang:send(Pid, {self(), CT3}),
-        io:format("sent: ct3~n", []),
+        io:format("[~p] sent: ct3~n", [ID]),
         timer:sleep(2_000),
         ok = sasl_auth:client_done(C)
     else
         Error ->
-            io:format("~0p~n", [Error])
+            io:format("[~p] ~0p~n", [ID, Error])
     end,
     ok.
 
-run_srv() ->
-    Service = env("SERVICE"),
+run_srv(ID, _CCname) ->
+
     maybe
         %% server always init from default keytab anyway,
         %% so doing kinit here doesn't really do anything
         %ok ?= sasl_auth:kinit("srv.keytab", env("SRV_PRINC")),
+
+        Service = env("SERVICE"),
         {ok, S} ?= sasl_auth:server_new(Service, env("SRV_PRINC")),
-        catch register(srv, self()),
+        register(srv_name(ID), self()),
         {Pid, CT1} =
             receive
                 {P0, T0} ->
@@ -56,20 +79,20 @@ run_srv() ->
                 10_000 ->
                     error(timeout)
             end,
-        io:format("got ct1: ~p~n", [CT1]),
+        io:format("[~p] got ct1: ~p~n", [ID, CT1]),
         {ok, {sasl_continue, ST1}} ?= sasl_auth:server_start(S, CT1),
-        CT2 = send_and_recv(Pid, st1, ST1),
-        io:format("got ct2~n", []),
+        CT2 = send_and_recv(ID, Pid, st1, ST1),
+        io:format("[~p] got ct2~n", [ID]),
         {ok, {sasl_continue, ST2}} ?= sasl_auth:server_step(S, CT2),
-        CT3 = send_and_recv(Pid, st2, ST2),
-        io:format("got ct3~n", []),
+        CT3 = send_and_recv(ID, Pid, st2, ST2),
+        io:format("[~p] got ct3~n", [ID]),
         {ok, {sasl_ok, ST3}} ?= sasl_auth:server_step(S, CT3),
-        io:format("sasl_ok: ~0p~n", [ST3]),
+        io:format("[~p] sasl_ok: ~0p~n", [ID, ST3]),
         <<>> = ST3,
         ok = sasl_auth:server_done(S)
     else
         Error ->
-            io:format("~0p~n", [Error])
+            io:format("[~p] ~0p~n", [ID, Error])
     end,
     ok.
 
@@ -83,10 +106,10 @@ env(Name) ->
             Val
     end.
 
-send_and_recv(Remote, Tag, Token) ->
+send_and_recv(ID, Remote, Tag, Token) ->
     Pid = self(),
     _ = erlang:send(Remote, {Pid, Token}),
-    io:format("sent: ~0p~n", [Tag]),
+    io:format("[~p] sent: ~0p~n", [ID, Tag]),
     receive
         {_Pid, Reply} ->
             Reply
@@ -95,13 +118,16 @@ send_and_recv(Remote, Tag, Token) ->
             error(timeout)
     end.
 
-wait_for_server() ->
-    case rpc:call(srv@srv, erlang, whereis, [srv]) of
+wait_for_server(Name) ->
+    case rpc:call(srv@srv, erlang, whereis, [Name]) of
         Pid when is_pid(Pid) ->
             io:format("\n", []),
             Pid;
         _ ->
             timer:sleep(1000),
             io:format(".", []),
-            wait_for_server()
+            wait_for_server(Name)
     end.
+
+srv_name(ID) ->
+    list_to_atom("srv_" ++ integer_to_list(ID)).
